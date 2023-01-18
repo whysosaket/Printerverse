b78c010d795b4b1eed827a4a4cd9ca3398743dbc
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-01-19 04:40:16 +0530

    Added Basic Page and Front Title

---
+++ number-tic-tac-toe/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <>
+    <div className='title'>
+      <h1><span className='jumpshake'>
+        <span style={{'color': "orange"}}>Tic</span> Tac
+        <span style={{'color': "orange"}}> Toe</span>
+        </span></h1>
+    </div>
+    </>
+  )
+}
+
+export default Home
